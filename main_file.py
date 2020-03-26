1c264fdf9c4f83ea53888c3d36de9768b0456322
BrahmanapallyNagaraju <44258487+BrahmanapallyNagaraju@users.noreply.github.com>
2020-03-26 17:35:49 +0530

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -2,8 +2,8 @@ import sys
 def is_even(num):
   num=int(num)
   if(num%2==0):
-    return true
-  return false
+    return True
+  return False
 print("Hello world!")
 print("after Integration")
 print("{} is even/odd:{}".format(sys.argv[1],is_even(sys.argv[1])))
