ac78450332fba3d53ea788fa745509fa25bef5e1
BrahmanapallyNagaraju <44258487+BrahmanapallyNagaraju@users.noreply.github.com>
2020-03-26 17:23:01 +0530

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -5,4 +5,4 @@ def is_even(num):
   return false
 print("Hello world!")
 print("after Integration")
-print("{} is even/odd: {}".format(sys.argv[1],is_even(int(sys.argv[1]))))
+print("{} is even/odd: {}".format(sys.argv[1],is_even(sys.argv[1])))
